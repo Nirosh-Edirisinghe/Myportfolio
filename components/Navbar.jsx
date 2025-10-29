cf4267910c16ec17b4f2237cfe10786f603db6fa
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-10-29 20:39:27 +0530

    add mobile menu to navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,22 @@
+'use client'
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Navbar = () => {
+  const sideMenuRef = useRef();
+
+  const openMenu = () => {
+    sideMenuRef.current.style.transform = 'translate(-16rem)'
+  }
+  const closeMenu = () => {
+    sideMenuRef.current.style.transform = 'translate(16rem)'
+  }
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-      <Image src={assets.header_bg_color} alt='' className='w-full'/>
-    </div>
+      <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt='' className='w-full' />
+      </div>
       <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="#top">
           <Image src={assets.logo} alt='' className='w-28 cursor-pointer mr-14' />
@@ -23,15 +32,26 @@ const Navbar = () => {
 
         <div className='flex items-center gap-3'>
           <button>
-            <Image src={assets.moon_icon} alt='' className='w-6'/>
+            <Image src={assets.moon_icon} alt='' className='w-6' />
           </button>
           <a href="#constct" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo'>Contact<Image src={assets.arrow_icon} alt='' className='w-3' /></a>
 
-          <button className='block md:hidden ml-3'>
-            <Image src={assets.menu_black} alt='' className='w-6'/>
+          <button onClick={openMenu} className='block md:hidden ml-3'>
+            <Image src={assets.menu_black} alt='' className='w-6' />
           </button>
         </div>
 
+        {/* Mobile menu */}
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
+          <div onClick={closeMenu} className='absolute right-6 top-6'>
+            <Image src={assets.close_black} alt='' className='w-4 cursor-pointer' />
+          </div>
+          <li onClick={closeMenu}><a className='font-ovo' href="#top">Home</a></li>
+          <li onClick={closeMenu}><a className='font-ovo' href="#about">About me</a></li>
+          <li onClick={closeMenu}><a className='font-ovo' href="#services">Services</a></li>
+          <li onClick={closeMenu}><a className='font-ovo' href="#work">My Work</a></li>
+          <li onClick={closeMenu}><a className='font-ovo' href="#contact">Contact me</a></li>
+        </ul>
       </nav>
     </>
   )
