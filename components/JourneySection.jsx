fc67b52f1b356c604a1d99e8d96fa4a30012ebf6
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-12-22 23:48:56 +0530

    change journey section styles

--- components/JourneySection.jsx
+++ components/JourneySection.jsx
@@ -1,6 +1,7 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
+import { motion } from "motion/react"
 
 const JourneySection = () => {
   const [lineHeight, setLineHeight] = useState("0%");
@@ -30,12 +31,29 @@ const JourneySection = () => {
   }, []);
 
   return (
-    <section
+    <motion.section
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
       id="journey-section"
       className="relative py-20"
     >
-      <div className="max-w-5xl mx-auto px-6">
-        <h2 className="text-4xl font-bold mb-16 text-center font-ovo">My Journey</h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="max-w-5xl mx-auto px-6">
+        <motion.h4
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.2 }}
+          className='text-center mb-2 text-lg font-ovo'>My Journey</motion.h4>
+
+        <motion.h2
+          initial={{ opacity: 0, y: -20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, delay: 0.5 }}
+          className="text-center text-5xl text-gray-800 font-ovo mb-6 dark:text-gray-200">Growth & Experience</motion.h2>
 
         <div className="flex flex-col space-y-24 relative">
           {/* ===== LEFT FULL LINE BACKGROUND ===== */}
@@ -52,17 +70,17 @@ const JourneySection = () => {
             <div className="flex flex-col items-center mr-8 relative z-10">
               <div className="bg-gray-200
                border-4 border-gray-700 w-14 h-14 rounded-full flex items-center justify-center shadow-lg">
-                <Image src={assets.esupport} alt="" className="w-8"/>
+                <Image src={assets.esupport} alt="" className="w-8" />
               </div>
             </div>
 
-            <div className="border-[0.5px] border-gray-200 rounded-2xl p-6 flex-1 shadow-md">
-              <h3 className="text-2xl font-semibold">
+            <div className="border-[0.5px] border-gray-300 rounded-2xl p-6 flex-1 shadow-md">
+              <h3 className="text-2xl text-gray-900 font-semibold dark:text-gray-300">
                 Software Engineering Intern
               </h3>
-              <p className="text-gray-800 mt-1">eSupport Technologies</p>
-              <p className="text-gray-600 mt-1">May - Nov 2025</p>
-              <p className="mt-4 text-gray-600">
+              <p className="text-gray-800 mt-1 dark:text-gray-400">eSupport Technologies (Pvt) Ltd</p>
+              <p className="text-gray-600 mt-1 dark:text-gray-400">May - Nov 2025</p>
+              <p className="mt-4 text-gray-600 dark:text-white">
                 Software Engineering Intern at eSupport Technologies, contributing to real-world web solutions using React, MUI, Laravel, and MySQL.Gained hands-on experience in building modern systems, improving performance, and working within a professional development team.
               </p>
             </div>
@@ -76,22 +94,22 @@ const JourneySection = () => {
               </div>
             </div>
 
-            <div className=" border-[0.5px] border-gray-200 rounded-2xl p-6 flex-1 shadow-md">
-              <h3 className="text-2xl font-semibold">
+            <div className=" border-[0.5px] border-gray-300 rounded-2xl p-6 flex-1 shadow-md">
+              <h3 className="text-2xl text-gray-900 font-semibold dark:text-gray-300">
                 Bachelor of Information and Communication Technology (Hons)
               </h3>
-              <p className="text-gray-800 mt-1">
+              <p className="text-gray-800 mt-1 dark:text-gray-400">
                 Rajarata University of Sri Lanka
               </p>
-              <p className="text-gray-600 mt-1">2021 – Present</p>
-              <p className="mt-4 text-gray-600">
+              <p className="text-gray-600 mt-1 dark:text-gray-400">2021 – Present</p>
+              <p className="mt-4 text-gray-600 dark:text-white">
                 A passionate ICT undergraduate at Rajarata University of Sri Lanka, focused on building modern digital solutions.Driven by curiosity and hands-on learning in software development and emerging technologies.Maintaining a strong academic performance with a current GPA of 3.48 while continuously growing in the tech field.
               </p>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </motion.div>
+    </motion.section>
   );
 };
 
